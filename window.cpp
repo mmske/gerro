5a7c6f52d3001cacc714e9d8527345b105edcd48
mmske <roland.gersei@gmail.com>
2017-05-12 17:39:40 +0200

    Add files via upload

---
+++ window.cpp
@@ -0,0 +1,90 @@
+#include "window.hpp"
+#include "statictext.hpp"
+#include "examplecheckbox.hpp"
+using namespace genv;
+
+Window::Window(int XX,int YY,int db,std::string neve):_XX(XX),_YY(YY),_db(db),_neve(neve)
+{
+    kov_jatekos=true;
+}
+
+void Window::feltolt()
+{
+    for(size_t i=0; i<_db; i++)
+    {
+        for(size_t j=0; j<_db; j++)
+        {
+            StaticText *tx[i*j];
+            ExampleCheckBox *ex[i*j];
+            ex[i*j]= new ExampleCheckBox(30*i,30*j,30,30);
+            tx[i*j]=new StaticText(30*i,30*j,30,30," ");
+            widgets.push_back(ex[i*j]);
+            widgets.push_back(tx[i*j]);
+        }
+    }
+}
+
+void Window::motor()
+{
+    for(size_t i=0;i<widgets.size();i++)
+        {
+            if((widgets*[i]->ex[i])->is_checked==true && kov_jatekos==true)
+            {
+                tx->setText(tx->value());
+                kov_jatekos==false;
+            }
+            if(ex->is_checked==true && kov_jatekos==false)
+            {
+                tx->setText(tx->value2());
+                kov_jatekos==true;
+            }
+        }
+}
+
+
+void Window::event_loop()
+{
+    gout.open(_XX,_YY);
+    gout.set_title(_neve);
+    event ev;
+    unsigned int focus = -1;
+    while(gin >> ev )
+    {
+        if (ev.type == ev_key && ev.keycode == key_tab)
+        {
+            if(focus!=-1)
+            {
+                widgets[focus]->unfocus();
+            }
+            do
+            {
+                focus++;
+                if (focus>=widgets.size()) focus =0;
+            }
+            while (!widgets[focus]->focusable());   //TODO:vegtelenciklus!
+            widgets[focus]->focus();
+        }
+        if (ev.type == ev_mouse && ev.button==btn_left)
+        {
+            for (unsigned int i=0; i<widgets.size(); i++)
+            {
+                if (widgets[i]->is_selected(ev.pos_x, ev.pos_y) && widgets[i]->focusable())
+                {
+                    if (focus!=-1) widgets[focus]->unfocus();
+                    focus = i;
+                    widgets[focus]->focus();
+                }
+            }
+        }
+        for (unsigned int i=0; i<widgets.size(); i++)
+        {
+            widgets[i]->draw();
+        }
+        if (focus!=-1)
+        {
+            widgets[focus]->handle(ev);
+        }
+        gout << refresh;
+    }
+}
+
